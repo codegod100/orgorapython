c60a6c7bb0efa39031245885419d74cd4d7ab43a
V <veronika.m.winters@gmail.com>
2023-11-11 23:26:59 -0800

    chore: Refactor import statements, function names, and crate name
    - Change import statement to use absolute path
    - Remove sum_as_string function
    - Rename orgorapython function to orgora
    - Update crate name in Cargo.toml
    
    ### BEGIN GIT COMMIT BEFORE AMEND
    
    ### END GIT COMMIT BEFORE AMEND

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
-use orgora::parse_file;
+use ::orgora::parse_file;
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
 
 #[pyfunction]
 fn parse_string(str: String) -> PyResult<String> {
@@ -14,7 +9,7 @@ fn parse_string(str: String) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn orgorapython(_py: Python, m: &PyModule) -> PyResult<()> {
+fn orgora(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(parse_string, m)?)?;
     Ok(())
 }
